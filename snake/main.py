ae58494ef93b026d7a4a4baf4cdca823367d5b76
Unknown <omoplus_3@hotmail.com>
2019-06-20 00:37:29 +0700

    back to rmsprop

--- snake/main.py
+++ snake/main.py
@@ -50,7 +50,7 @@ def plot_rewards():
 
 policy_net = convnet(config.BOARD_SIZE, in_channel=5).float().to(device, non_blocking=True).eval()
 target_net = convnet(config.BOARD_SIZE, in_channel=5).float().to(device, non_blocking=True).eval()
-optimizer = torch.optim.Adam(policy_net.parameters(), lr=1e-4)
+optimizer = torch.optim.RMSprop(policy_net.parameters(), lr=1e-4, momentum=0.9)
 scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones= [25000,50000,125000,250000,1250000], gamma=0.5)
 
 env = Snake(config.BOARD_SIZE)
