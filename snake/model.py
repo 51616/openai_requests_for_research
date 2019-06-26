5abfef8a1fabf98e976e1094639bf8d5fb3163b1
Unknown <omoplus_3@hotmail.com>
2019-06-26 16:37:52 +0700

    Revert "dueling dqn"
    This reverts commit cad74917147edc53feb46a93599569b6cfc37f55.

--- snake/model.py
+++ snake/model.py
@@ -45,28 +45,3 @@ class convnet(nn.Module):
         x = self.dense(x.view(-1,self.conv_channel*self.board_size**2))
         # print('Model output:',x)
         return x
-
-class convnet_duel(nn.Module):
-    def __init__(self, board_size=20, hidden_size=128, conv_channel = 64, in_channel=3):
-        super(convnet_duel, self).__init__()
-        self.hidden_size = hidden_size
-        self.board_size = board_size
-        self.conv_channel = conv_channel
-        self.in_channel = in_channel
-        self.conv1 = nn.Conv2d(self.in_channel,self.conv_channel//2,kernel_size=3,stride=1,padding=1)
-        self.bn1 = nn.BatchNorm2d(self.conv_channel//2)
-        self.conv2 = nn.Conv2d(self.conv_channel//2,self.conv_channel,kernel_size=3,stride=1,padding=1)
-        self.bn2 = nn.BatchNorm2d(self.conv_channel)
-        self.dense = nn.Linear(self.conv_channel*self.board_size**2, self.hidden_size)
-        self.advantage_head = nn.Linear(self.hidden_size, 4)
-        self.value_head = nn.Linear(self.hidden_size, 1)
-
-    def forward(self,x):
-        x = x.view(-1,self.in_channel,self.board_size,self.board_size).float()
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = self.dense(x.view(-1,self.conv_channel*self.board_size**2))
-        advantage = self.advantage_head(x)
-        value = self.value_head(x)
-        output = value + advantage - advantage.mean(1, keepdim=True) #Dueling DQN
-        return output
