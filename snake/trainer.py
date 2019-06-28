1cf3fb752b0cb725b4aa639ef297359f2b3488aa
Unknown <omoplus_3@hotmail.com>
2019-06-28 17:41:06 +0700

    single step scheduler

--- snake/trainer.py
+++ snake/trainer.py
@@ -114,7 +114,7 @@ def optimize_model(policy_net, target_net, replay_memory, optimizer, scheduler, 
     for param in policy_net.parameters():
         param.grad.data.clamp_(-1, 1)
     optimizer.step()
-    # scheduler.step()
+    scheduler.step()
     policy_net.eval()
     # print('Model mode:',policy_net.training)
     return
