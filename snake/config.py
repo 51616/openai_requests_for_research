955438d45f4b9b4949278d485c03f31660bdff41
Unknown <omoplus_3@hotmail.com>
2019-06-28 20:17:35 +0700

    9x9

--- snake/config.py
+++ snake/config.py
@@ -3,9 +3,9 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # device = 'cpu'
 
-BOARD_SIZE = 7
+BOARD_SIZE = 9
 
-TOTAL_STEPS = 2000000
+TOTAL_STEPS = 10000000
 STEP_SIZE = 4
 BATCH_SIZE = 512
 GAMMA = 0.98
@@ -15,5 +15,5 @@ EPS_DECAY = 10000
 TARGET_UPDATE = 100
 SHOW_IT = 10000
 START_N_STEPS = 1
-MAX_N_STEPS = 20
+MAX_N_STEPS = 10
 N_STEPS_UPDATE = 100000
