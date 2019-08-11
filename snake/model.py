aaeac3ffb506fe5bd83ba199aac502dc3f0acb59
Unknown <omoplus_3@hotmail.com>
2019-08-11 22:43:19 +0700

    add resnet

--- snake/model.py
+++ snake/model.py
@@ -25,6 +25,37 @@ class feedforward(nn.Module):
         x = self.dense3(x)
         return x
 
+
+class ResBlock(nn.Module):
+    def __init__(self, filters=64, kernel=3):
+        super(ResBlock, self).__init__()
+        if kernel % 2 != 1:
+            raise ValueError('kernel must be odd, got %d' % kernel)
+        # pad = int(np.floor(kernel/2))
+
+        self.conv1 = nn.Conv2d(
+            filters, filters, kernel_size=kernel, padding=0)
+        self.bn1 = nn.BatchNorm2d(filters)
+        self.conv2 = nn.Conv2d(
+            filters, filters, kernel_size=kernel, padding=0)
+        self.bn2 = nn.BatchNorm2d(filters)
+        self.negative_pad = nn.ConstantPad2d(1, -1)
+
+    def forward(self, x):
+        inp = x
+
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.conv1(self.negative_pad(x))
+
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = self.conv2(self.negative_pad(x))
+
+        x = x + inp
+
+        return x
+
 class convnet(nn.Module):
     def __init__(self, board_size=20, hidden_size=128, conv_channel = 64, in_channel=3, last_conv_channel=32):
         super(convnet, self).__init__()
@@ -37,6 +68,8 @@ class convnet(nn.Module):
 
         self.conv1 = nn.Conv2d(self.in_channel,self.conv_channel,kernel_size=3,stride=1,padding=0)
         self.bn1 = nn.BatchNorm2d(self.conv_channel)
+
+
         self.conv2 = nn.Conv2d(self.conv_channel,self.conv_channel,kernel_size=3,stride=1,padding=0)
         self.bn2 = nn.BatchNorm2d(self.conv_channel)
 
@@ -51,5 +84,39 @@ class convnet(nn.Module):
         x = F.relu(self.bn2(self.conv2(self.negative_pad(x))))
         x = F.relu(self.bn3(self.conv3(x)))
         x = self.dense(x.view(-1,self.last_conv_channel*self.board_size**2))
+        # print('Model output:',x)
+        return x
+
+
+class ResNet(nn.Module):
+    def __init__(self, board_size=20, hidden_size=256, conv_channel=64, in_channel=3, last_conv_channel=32, blocks=5):
+        super(ResNet, self).__init__()
+        self.hidden_size = hidden_size
+        self.board_size = board_size
+        self.conv_channel = conv_channel
+        self.in_channel = in_channel
+        self.last_conv_channel = last_conv_channel
+        self.blocks = blocks
+        self.negative_pad = nn.ConstantPad2d(1, -1)
+
+        self.init_conv = nn.Conv2d(self.in_channel,self.conv_channel,kernel_size=3,stride=1,padding=0)
+        self.bn1 = nn.BatchNorm2d(self.conv_channel)
+
+        self.res_blocks = nn.ModuleList([ResBlock(filters=conv_channel) for i in range(blocks-1)])
+
+        self.last_conv = nn.Conv2d(self.conv_channel, self.last_conv_channel, kernel_size=1)
+        self.bn2 = nn.BatchNorm2d(self.last_conv_channel)
+
+        self.hidden = nn.Linear(self.last_conv_channel*self.board_size**2, self.hidden_size)
+        self.q = nn.Linear(self.hidden_size, 4)
+
+    def forward(self,x):
+        x = x.view(-1,self.in_channel,self.board_size,self.board_size).float()
+        x = F.relu(self.bn1(self.init_conv(self.negative_pad(x))))
+        for i in range(self.blocks-1):
+            x = self.res_blocks[i](x)
+        x = F.relu(self.bn2(self.last_conv(x)))
+        x = self.hidden(x.view(-1,self.last_conv_channel*self.board_size**2))
+        x = self.q(x)
         # print('Model output:',x)
         return x
